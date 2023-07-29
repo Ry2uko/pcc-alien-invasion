4674837b8889be56f7e5678dbb5227efca7313ac
Ry2uko <83095832+Ry2uko@users.noreply.github.com>
2023-07-28 17:42:34 -0700

    Chapter 13: Aliens!
    fleet of aliens, moving the aliens, repopulating, statistics, and game over

--- settings.py
+++ settings.py
@@ -12,10 +12,16 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 0.8
+        self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.2
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
+
+        # Alien settings
+        self.alien_speed = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1  # 1 - right, -1 - left
